4a089a90d4c8ba1fbc01900505b1483ac75c90c0
villesalmela <contact@villesalmela.fi>
2017-03-13 20:49:41 +0200

    Created new branch: sketch

--- RecipeTool.java
+++ RecipeTool.java
@@ -3,7 +3,8 @@ package recipeTool;
 public class RecipeTool {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("New branch: sketch");
 
 	}
 
